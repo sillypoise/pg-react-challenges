679545bfef0c9067e4aa0a26977e977c2f691e40
sillypoise <somewhatjose@gmail.com>
2022-10-19 09:31:30 -0500

    p/generic-task-app_solution: Added mock api with mock data

---
+++ project/generic-task-app/src/api.ts
@@ -0,0 +1,29 @@
+import { Item } from "./types";
+
+let api = {
+    list: (): Promise<Array<Item>> => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve([
+                    {
+                        id: 1,
+                        text: "Some thing to buy",
+                        completed: false,
+                    },
+                    {
+                        id: 2,
+                        text: "Some other thing to buy",
+                        completed: true,
+                    },
+                    {
+                        id: 3,
+                        text: "Some last to buy",
+                        completed: false,
+                    },
+                ]);
+            }, 1000);
+        });
+    },
+};
+
+export { api };
