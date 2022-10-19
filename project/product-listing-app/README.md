0657058f5fa8a7ce55d6bacded9d4213bcc3419e
sillypoise <somewhatjose@gmail.com>
2022-10-19 12:43:51 -0500

    p/product-listing-app_solution: listed fetched products in ProductList component

--- project/product-listing-app/README.md
+++ project/product-listing-app/README.md
@@ -1,9 +1,9 @@
 # Tasks to solve
 
--   [ ] make sure products load when app starts
+-   [x] make sure products load when app starts
 -   [ ] Implement product search functionality
 -   [ ] Products with price <= 100 should have a `sale` class activated
--   [ ] Add a loading state to app whilst it fetches product listing
+-   [x] Add a loading state to app whilst it fetches product listing
 -   [ ] Make sure search is case insensitive
 -   [ ] Can you localize prices to your country's currency
 -   [ ] Add a select to sort results alphabetically or by price
--- project/product-listing-app/src/App.tsx
+++ project/product-listing-app/src/App.tsx
@@ -1,15 +1,45 @@
 import { useEffect, useState } from "react";
 import { api } from "./api";
+import { Product } from "./types";
+
+function ProductList({ products }: { products: Array<Product> }) {
+    return (
+        <ul
+            role="list"
+            className="center [--center-width:theme(contentWidth.3)] auto-grid"
+        >
+            {products.map((product) => (
+                <li key={product.id} className="box">
+                    <h4>{product.title}</h4>
+                    <p>{product.description}</p>
+                    <span>${product.price}</span>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 function App() {
+    let [products, setProducts] = useState<Array<Product>>([]);
+    let [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-        api.search().then((data) => console.dir(data));
+        api.search().then((data) => {
+            setProducts(data);
+            setIsLoading(false);
+        });
     }, []);
 
     return (
         <main className="mlb-l">
             <article className="stack center">
                 <h1 className="text-3">Product listing app</h1>
+                <hr />
+                {isLoading ? (
+                    <p>Loading your products...</p>
+                ) : (
+                    <ProductList products={products} />
+                )}
             </article>
         </main>
     );
