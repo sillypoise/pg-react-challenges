82cc4fa8ca650c56691b19276907442e70733cd0
sillypoise <somewhatjose@gmail.com>
2022-10-19 12:06:17 -0500

    p/product-listing-app: created project, added styles, added scripts

---
+++ project/product-listing-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <main className="mlb-l">
+            <article className="stack center">
+                <h1 className="text-3">Product listing app</h1>
+            </article>
+        </main>
+    );
+}
+
+export default App;
