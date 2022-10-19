8ba14c7ea6ed1ed02203e7fa7ae82fe674ee82a2
sillypoise <somewhatjose@gmail.com>
2022-10-19 10:20:08 -0500

    p/generic-task-app_solution: Added delete task functionality

--- project/generic-task-app/src/App.tsx
+++ project/generic-task-app/src/App.tsx
@@ -2,13 +2,22 @@ import { useEffect, useState } from "react";
 import { api } from "./api";
 import { Item } from "./types";
 
-function TaskList({ items }: { items: Array<Item> }) {
+function TaskList({
+    items,
+    handleDelete,
+}: {
+    items: Array<Item>;
+    handleDelete: (id: Item["id"]) => void;
+}) {
+    if (!items.length) {
+        return <p>Seems like you're done with your tasks! ✌️</p>;
+    }
     return (
         <ul role="list" className="stack">
             {items.map((item) => (
                 <li key={item.id} className="cluster gap-s">
                     <span>{item.text}</span>
-                    <button>❌</button>
+                    <button onClick={() => handleDelete(item.id)}>❌</button>
                 </li>
             ))}
         </ul>
@@ -17,20 +26,29 @@ function TaskList({ items }: { items: Array<Item> }) {
 
 function App() {
     let [items, setItems] = useState<Array<Item>>([]);
+    let [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        api.list().then((data) => setItems(data));
+        api.list().then((data) => {
+            setIsLoading(false);
+            setItems(data);
+        });
     }, []);
+
+    function handleDelete(id: Item["id"]) {
+        let filteredItems = items.filter((item) => item.id !== id);
+        setItems(filteredItems);
+    }
 
     return (
         <main className="mlb-l">
             <article className="center stack">
                 <h1 className="text-3">Generic Task App</h1>
                 <hr />
-                {!items.length ? (
+                {isLoading ? (
                     <p>Loading your data...</p>
                 ) : (
-                    <TaskList items={items} />
+                    <TaskList items={items} handleDelete={handleDelete} />
                 )}
             </article>
         </main>
