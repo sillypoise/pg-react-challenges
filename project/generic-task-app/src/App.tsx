119331167e0565adc0011ff7316b63f06a769b3e
sillypoise <somewhatjose@gmail.com>
2022-10-19 09:03:57 -0500

    p/generic-task-app: initiated project, added scripts, added tailwind, converted to commonjs

---
+++ project/generic-task-app/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <main>
+            <article>
+                <h1>Generic Task App</h1>
+            </article>
+        </main>
+    );
+}
+
+export default App;
