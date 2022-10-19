31a7113124ffdaaa0a03bc87a3c033b2e375bd24
sillypoise <somewhatjose@gmail.com>
2022-10-19 09:39:41 -0500

    p/generic-task-app_solution: display task list from api

--- project/generic-task-app/README.md
+++ project/generic-task-app/README.md
@@ -3,8 +3,8 @@
 You will need to create the following features:
 
 -   [ ] An input where we can write and add a new task
--   [ ] Display a list of tasks
--   [ ] Write a mocked API with delayed response for grabbing initial tasks
+-   [x] Display a list of tasks
+-   [x] Write a mocked API with delayed response for grabbing initial tasks
 -   [ ] Ability to add a task
 -   [ ] Ability to delete a task
 -   [ ] Ability to toggle a task between completed and not completed
--- project/generic-task-app/src/App.tsx
+++ project/generic-task-app/src/App.tsx
@@ -1,15 +1,37 @@
 import { useEffect, useState } from "react";
 import { api } from "./api";
+import { Item } from "./types";
+
+function TaskList({ items }: { items: Array<Item> }) {
+    return (
+        <ul role="list" className="stack">
+            {items.map((item) => (
+                <li key={item.id} className="cluster gap-s">
+                    <span>{item.text}</span>
+                    <button>❌</button>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 function App() {
+    let [items, setItems] = useState<Array<Item>>([]);
+
     useEffect(() => {
-        api.list().then((data) => console.dir(data));
+        api.list().then((data) => setItems(data));
     }, []);
 
     return (
         <main className="mlb-l">
             <article className="center stack">
                 <h1 className="text-3">Generic Task App</h1>
+                <hr />
+                {!items.length ? (
+                    <p>Loading your data...</p>
+                ) : (
+                    <TaskList items={items} />
+                )}
             </article>
         </main>
     );
