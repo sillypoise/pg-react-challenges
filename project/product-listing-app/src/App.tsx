f63076075244968bd04f3f8601bf15870ad42f2e
sillypoise <somewhatjose@gmail.com>
2022-10-21 17:26:40 -0500

    p/product-listing-app_solution-2: correctly fetching products

--- project/product-listing-app/src/App.tsx
+++ project/product-listing-app/src/App.tsx
@@ -4,11 +4,26 @@ import { Product } from "./types";
 import { z } from "zod";
 
 function App() {
+    let [products, setProducts] = useState<Array<Product>>([]);
+    let [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        api.list("").then((data) => {
+            setProducts(data);
+            setIsLoading(false);
+        });
+    }, []);
     return (
         <main className="mlb-l">
             <article className="stack center">
                 <h1 className="text-3">Product listing app</h1>
                 <hr />
+                {isLoading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <pre>{JSON.stringify(products, null, 4)}</pre>
+                )}
             </article>
         </main>
     );
--- project/product-listing-app/src/api.ts
+++ project/product-listing-app/src/api.ts
@@ -1,39 +1,51 @@
+import { Product } from "./types";
+
 let api = {
-    list: () => {
-        return [
-            {
-                id: 1,
-                title: "TV 55 pulgadas",
-                description: "TV 55 pulgadas, curva, marca gonzung",
-                price: 1000,
-            },
-            {
-                id: 2,
-                title: "Webcam para streams",
-                description: "Webcam marca logitency",
-                price: 200,
-            },
-            {
-                id: 3,
-                title: "Celular gama alta",
-                description: "Celular gama alta, marca xiaoncy",
-                price: 300,
-            },
-            {
-                id: 4,
-                title: "Dron profesional",
-                description:
-                    "Dron que soporta fuertes vientos, marca phantoncy",
-                price: 500,
-            },
-            {
-                id: 5,
-                title: "Ventilador de escritorio",
-                description:
-                    "Ventilador portable de escritorio, marca liliancy",
-                price: 50,
-            },
-        ];
+    list: (query: string): Promise<Array<Product>> => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(
+                    [
+                        {
+                            id: 1,
+                            title: "TV 55 pulgadas",
+                            description: "TV 55 pulgadas, curva, marca gonzung",
+                            price: 1000,
+                        },
+                        {
+                            id: 2,
+                            title: "Webcam para streams",
+                            description: "Webcam marca logitency",
+                            price: 200,
+                        },
+                        {
+                            id: 3,
+                            title: "Celular gama alta",
+                            description: "Celular gama alta, marca xiaoncy",
+                            price: 300,
+                        },
+                        {
+                            id: 4,
+                            title: "Dron profesional",
+                            description:
+                                "Dron que soporta fuertes vientos, marca phantoncy",
+                            price: 500,
+                        },
+                        {
+                            id: 5,
+                            title: "Ventilador de escritorio",
+                            description:
+                                "Ventilador portable de escritorio, marca liliancy",
+                            price: 50,
+                        },
+                    ].filter((product) => {
+                        return product.title
+                            .toLowerCase()
+                            .includes(query.toLowerCase());
+                    })
+                );
+            }, 1000);
+        });
     },
 };
 export { api };
