76b122f3f0d561ec1b7852a42933ed8fa85b9dee
sillypoise <somewhatjose@gmail.com>
2022-10-23 16:47:46 -0500

    p/wordle_solution: wrote keyboard input logic

--- project/wordle/src/App.tsx
+++ project/wordle/src/App.tsx
@@ -1,19 +1,70 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { api } from "./api";
 
 function App() {
     const answer = "RIGHT";
     let [words, setWords] = useState<Array<Array<string>>>(() =>
-        Array.from({ length: 6 }, () => new Array(5).fill("hi"))
+        Array.from({ length: 6 }, () => new Array(5).fill(""))
+    );
+    let [turn, setTurn] = useState(0);
+    let [status, setStatus] = useState<"playing" | "finished">("playing");
+
+    let handleKeyDown = useCallback(
+        function handleKeyDown(e: KeyboardEvent) {
+            switch (e.key) {
+                case "Enter": {
+                    if (words[turn].join("") === answer) {
+                        setStatus("finished");
+                    }
+
+                    setTurn((turn) => turn + 1);
+
+                    return;
+                }
+                case "Backspace": {
+                    e.preventDefault();
+                    let firstEmptyIndex = words[turn].findIndex(
+                        (letter) => letter === ""
+                    );
+
+                    if (firstEmptyIndex === -1) {
+                        firstEmptyIndex = words[turn].length;
+                    }
+
+                    words[turn][firstEmptyIndex - 1] = "";
+
+                    setWords(words.slice());
+
+                    return;
+                }
+                default: {
+                    if (e.key.length === 1 && e.key.match(/[a-z]/i)) {
+                        const firstEmptyIndex = words[turn].findIndex(
+                            (letter) => letter === ""
+                        );
+
+                        if (firstEmptyIndex === -1) return;
+
+                        words[turn][firstEmptyIndex] = e.key.toUpperCase();
+
+                        setWords(words.slice());
+
+                        return;
+                    }
+                }
+            }
+        },
+        [turn, words, answer]
     );
 
-    // useEffect(() => {
-    //     api.random((data) => setWords(data))
-    // }, [words])
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
+    }, [handleKeyDown]);
 
     return (
         <main className="mlb-l">
-            <article className="center stack board">
+            <article className="board ">
                 {words.map((word, wordIndex) => (
                     <section key={wordIndex} className="word">
                         {word.map((letter, letterIndex) => {
@@ -28,6 +79,10 @@ function App() {
                         })}
                     </section>
                 ))}
+                <div className="grid">
+                    <span>turn: {turn}</span>
+                    <span>status: {status}</span>
+                </div>
             </article>
         </main>
     );
