b1c2d08e30d899a597bfba59d00fb4cc0a5c575c
sillypoise <somewhatjose@gmail.com>
2022-10-25 10:30:18 -0500

    p/pokemon_solution: working on using a Map instead of an Array for cart state

--- project/pokemon/src/App.tsx
+++ project/pokemon/src/App.tsx
@@ -1,41 +1,75 @@
-import { useState } from "react";
+import React, { memo, useCallback, useState } from "react";
 import "nes.css/css/nes.min.css";
 import "../styles/tailwind.css";
 
 import { POKEMONS } from "./constants";
 import { Pokemon } from "./types";
 
-function PokemonCard({ pokemon }: { pokemon: Pokemon }) {
+let PokemonCard = memo(function PokemonCard({
+    pokemon,
+    onAdd,
+    setCart,
+}: {
+    pokemon: Pokemon;
+    onAdd: (pokmon: Pokemon) => void;
+    setCart: React.Dispatch<React.SetStateAction<Array<Pokemon>>>;
+}) {
     return (
-        <article key={pokemon.id}>
+        <article
+            key={pokemon.id}
+            className="box stack [--stack-gap:theme(spacing.3xs)]"
+        >
             <img
                 src={pokemon.image}
                 alt={pokemon.description}
                 className="nes-container"
             />
-            <div className="cluster">
-                <p className="text-1">{pokemon.name}</p>
-                <p className="text-0">${pokemon.price}</p>
-            </div>
-            <button className="nes-btn">Add to cart</button>
+            <p className="text-1">{pokemon.name}</p>
+            <p className="text-0">${pokemon.price}</p>
+            <button
+                onClick={() => onAdd(pokemon)}
+                // onClick={() => setCart((cart) => cart.concat(pokemon))}
+                className="nes-btn"
+            >
+                Add to cart
+            </button>
         </article>
     );
-}
+});
 
 function App() {
-    let [cart, setCart] = useState<Array<Pokemon>>([]);
+    let [cart, setCart] = useState<Map<Pokemon["id"], number>>(() => new Map());
+
+    let onAdd = useCallback(function onAdd(pokemon: Pokemon) {
+        let prevQty = cart.get(pokemon.id);
+        setCart((cart) =>
+        // TODO: Use Map instead of object. Make new copy of map each time with setCart
+            // cart.set(pokemon.id, cart.set(pokemon.id, prevQty + 1))
+        );
+    }, []);
+
+    console.dir(cart);
+
     return (
         <main className="mlb-l">
             <article className="stack center">
                 <h1 className="text-3">Pokemon Store</h1>
-                <div className="sidebar">
+                <div>
                     <section className="auto-grid">
                         {POKEMONS.map((pokemon) => (
-                            <PokemonCard key={pokemon.id} pokemon={pokemon} />
+                            <PokemonCard
+                                key={pokemon.id}
+                                pokemon={pokemon}
+                                // onAdd={() =>
+                                //     setCart((cart) => cart.concat(pokemon))
+                                // }
+                                onAdd={onAdd}
+                                // setCart={setCart}
+                            />
                         ))}
                     </section>
-                    <pre>{JSON.stringify(cart, null, 4)}</pre>
                 </div>
+                <pre>{JSON.stringify(Object.fromEntries(cart), null, 4)}</pre>
                 <aside>
                     <button className="nes-btn is-primary">
                         0 items (total $0)
